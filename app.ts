9de0eb14e4e9053001e099a4f1e3b6228b83537e
SantiCoVe <cottevelasquez@gmail.com>
2025-04-24 15:18:32 -0500

    feat: Se modifico una parte del app

--- app.ts
+++ app.ts
@@ -12,14 +12,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express().use(bodyParser.json());
-
+// rutas usuario
 app.use('/register', register);
 app.use('/auth', auth);
 app.use('/profile', profile);
 app.use('/start', estado_camion);
 app.use('/requests', solicitudes);
+// rutas admin
 app.use('/authAdmin', authAdmin);
-app.use('/startAdmin', )
+//app.use('/startAdmin', )
 
 const PORT = process.env.PORT || 10101;
 
