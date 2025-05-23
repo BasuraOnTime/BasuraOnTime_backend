2f9daaf2fd09856c6fa83cefe157121ed03f9032
SantiCoVe <cottevelasquez@gmail.com>
2025-05-23 17:48:29 -0500

    feat: se modifico un apartado del perfil

--- controllers/Usuario-controller/profile-controller.ts
+++ controllers/Usuario-controller/profile-controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import UserService from "../../services/Usuario/UserServices";
-import { database } from "firebase-admin";
 
 let profile = async (req: Request, res: Response) => {
   try {
